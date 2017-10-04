1151531ceef5a0f74f3ced0c70d7ad03c56f742b
Standuply <hello@standuply.com>
2017-10-04 12:17:54 +0700

    Giphy switched to API.

--- requests/giphy.js
+++ requests/giphy.js
@@ -1,67 +1,62 @@
-const requestPromise = require('request-promise');
+const NodeCache = require("node-cache");
+const myCache = new NodeCache({stdTTL: 100, checkperiod: 120});
+
+// Require with the public beta key
+const giphy = require('giphy-api')();
 
 // Stub for error response
 const errorMessage = require('../response-stubs/error');
 
 // This function parses giphy.com using simple strings
-const parsePage = (data) => {
-    const num = Math.floor(Math.random() * 10) + 1;
+const processGiphy = (data) => {
+    let gifURL;
+    const num = Math.floor(Math.random() * data.data.length);
 
-    let startIndex = 0;
-    for (let i = 0; i < num; i++) {
-        startIndex = data.indexOf('"fixed_height":', startIndex + 1);
+    if (data.data[num].images.fixed_height.size > 2500000) {
+        gifURL = data.data[num].images.fixed_height_small.url;
+    } else {
+        gifURL = data.data[num].images.fixed_height.url;
     }
 
-    let gifsIndex = startIndex;
-    let gifsString = data.substring(data.indexOf('{', gifsIndex), data.indexOf('}', gifsIndex) + 1);
-    let gifs = JSON.parse(gifsString);
+    let stillURL = data.data[num].images.fixed_height_small_still.url;
 
-    // check size to fit the Slack's 3MB limit
-    if (gifs && gifs.size && gifs.size > 2500000) {
-        gifsIndex = data.indexOf('"fixed_height_small":', startIndex + 1);
-        gifsString = data.substring(data.indexOf('{', gifsIndex), data.indexOf('}', gifsIndex) + 1);
-        gifs = JSON.parse(gifsString);
-
-    }
-
-    const stillIndex = data.indexOf('"fixed_height_small_still":', startIndex + 1);
-    const stillString = data.substring(data.indexOf('{', stillIndex), data.indexOf('}', stillIndex) + 1);
-    const still = JSON.parse(stillString);
-
-    return {gifs, still};
+    return {image: gifURL, thumbnail: stillURL};
 };
 
 // This function gets and parses giphy.com using simple strings
 module.exports = (req, res, next) => {
-    const request = {
-        method: 'GET',
-        uri: 'https://giphy.com/',
-        json: false,
-        simple: false
-    };
-
-    requestPromise(request)
-        .then(data => {
-            const parseResult = parsePage(data);
-
-            // Make the Slack attachment - one object
-            const result = {
-                fallback: 'Giphy daily digest.',
-                color: '#36a64f',
-                pretext: 'A trending gif',
-                title: 'Giphy',
-                title_link: 'https://giphy.com/',
-                image_url: parseResult.gifs.url,
-                thumb_url: parseResult.still.url,
-                footer: 'Standuply',
-                footer_icon: 'https://app.standuply.com/img/16.png',
-                ts: Math.round(Date.now() / 1000)
-            };
-            res.json(result);
+    let value = myCache.get("giphy");
+    if (value === undefined) {
+        giphy.trending({
+            limit: 10,
+            fmt: 'json'
         })
-        .catch(error => {
-            console.log('giphy error', error);
-            res.json(errorMessage(error.toString()));
-        });
+            .then(data => {
+                const parseResult = processGiphy(data);
+
+                // Make the Slack attachment - one object
+                const result = {
+                    fallback: 'Giphy daily digest.',
+                    color: '#36a64f',
+                    pretext: 'A trending gif',
+                    title: 'Giphy',
+                    title_link: 'https://giphy.com/',
+                    image_url: parseResult.image,
+                    thumb_url: parseResult.thumbnail,
+                    footer: 'Standuply',
+                    footer_icon: 'https://app.standuply.com/img/16.png',
+                    ts: Math.round(Date.now() / 1000)
+                };
+
+                myCache.set("giphy", result);
+                res.json(result);
+            })
+            .catch(error => {
+                console.log('giphy error', error);
+                res.json(errorMessage(error.toString()));
+            });
+    } else {
+        res.json(value);
+    }
 
 };
