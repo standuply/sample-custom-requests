ca1f339212771a968cbf803ec53aeabf7e0484c4
Standuply <hello@standuply.com>
2017-10-04 12:16:21 +0700

    Producthunt switched to API.

---
+++ config/producthunt.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    client_id: 'CLIENT_ID', // your client_id
+    client_secret: 'CLIENT_SECRET', // your client_secret
+    grant_type: 'client_credentials'
+};
+
--- requests/producthunt.js
+++ requests/producthunt.js
@@ -1,84 +1,79 @@
-const requestPromise = require('request-promise');
-const cheerio = require('cheerio');
-const emoji = require('node-emoji');
+const NodeCache = require("node-cache");
+const myCache = new NodeCache({stdTTL: 100, checkperiod: 120});
+
+const productHuntAPI = require('producthunt');
+const productHuntConfig = require('../config/producthunt.config');
 
 // Stub for error response
 const errorMessage = require('../response-stubs/error');
 
-//This function parses producthunt.com using Cheerio.js
-const parsePage = (data) => {
-    const page = cheerio.load(data);
-    const fullWidthBox = cheerio.load(page('*[class^="fullWidthBox_"]').html());
-    const primContent = cheerio.load(fullWidthBox('*[class^="postsList_"]').html());
-
+//This function parses producthunt's API response
+const processResponse = (data) => {
     const fields = [];
     let thumbUrl = 'https://s3.producthunt.com/static/ph-logo-2.png';
 
-    primContent('li').each((i, elm) => {
-        const itemContent = cheerio.load(primContent(elm).html(), {decodeEntities:false});
-        const href = itemContent('*[class*="link_"]').attr('href');
-        if (!thumbUrl) {
-            const imgTag = cheerio.load(itemContent('*[class*="thumbnail_"]').find('noscript').html());
-            thumbUrl = imgTag('img').attr('src');
-        }
-
-        const title = itemContent('*[class*="title_"]').html();
-        const tagline = emoji.unemojify( itemContent('*[class*="tagline_"]').html() );
-        const actions = itemContent('*[class*="actions_"]').text();
-        const actionsArray = actions.split(/(\s+)/);
-
-        let upvotes = '';
-        let comments = '';
-        if (actionsArray.length >= 3) {
-            upvotes = actionsArray[actionsArray.length - 3];
-            comments = actionsArray[actionsArray.length - 1];
-        }
+    for (let i = 0; i < 5; i++) {
+        const post = data.posts[i];
 
         // Prepare fields array according to Slack attachment format
         fields.push({
-            title: title + '  :small_red_triangle: ' + upvotes + '  :speech_balloon: ' + comments,
-            value: '<https://www.producthunt.com' + href + '|' + tagline + '>',
+            title: post.name + '  :small_red_triangle: ' + post.votes_count + '  :speech_balloon: ' + post.comments_count,
+            value: `<${post.discussion_url}|${post.tagline}>`,
             short: false
         });
-
-        if (i >= 4) return false;
-    });
+    }
 
     return {fields, thumbUrl};
 };
 
-// This function gets producthunt.com and generates Slack attachment
+let responseWithError = function (error, res) {
+    console.log('Product Hunt error', error);
+    res.json(errorMessage(error));
+};
+
+// This function gets last posts from producthunt.com and generates Slack attachment
 module.exports = (req, res, next) => {
-    const request = {
-        method: 'GET',
-        uri: 'https://www.producthunt.com/',
-        encoding : 'utf8',
-        json: false,
-        simple: false
-    };
+    let value = myCache.get("producthunt");
+    if (value === undefined) {
 
-    requestPromise(request)
-        .then(data => {
-            const parseResult = parsePage(data);
+        const productHunt = new productHuntAPI(productHuntConfig);
 
-            // Make the Slack attachment - one object
-            const result = {
-                fallback: 'Product Hunt daily digest.',
-                color: '#36a64f',
-                pretext: 'Top products on Product Hunt',
-                title: 'Product Hunt popular',
-                title_link: 'https://www.producthunt.com/',
-                fields: parseResult.fields,
-                thumb_url: parseResult.thumbUrl,
-                mrkdwn_in: ['text', 'fields'],
-                footer: 'Standuply',
-                footer_icon: 'https://app.standuply.com/img/16.png',
-                ts: Math.round(Date.now() / 1000)
-            };
-            res.json(result);
-        })
-        .catch(error => {
-            console.log('Product Hunt error', error);
-            res.json(errorMessage(error.toString()));
+        productHunt.posts.index({}, (error, postsResult) => {
+            if (error) {
+                responseWithError(error.toString(), res);
+                return;
+            }
+
+            const resultJSON = postsResult.toJSON();
+            if (resultJSON.statusCode < 300) {
+                const bodyJSON = JSON.parse(resultJSON.body);
+
+                const parseResult = processResponse(bodyJSON);
+
+                // Make the Slack attachment - one object
+                const result = {
+                    fallback: 'Product Hunt daily digest.',
+                    color: '#36a64f',
+                    pretext: 'Top products on Product Hunt',
+                    title: 'Product Hunt popular',
+                    title_link: 'https://www.producthunt.com/',
+                    fields: parseResult.fields,
+                    thumb_url: parseResult.thumbUrl,
+                    mrkdwn_in: ['text', 'fields'],
+                    footer: 'Standuply',
+                    footer_icon: 'https://app.standuply.com/img/16.png',
+                    ts: Math.round(Date.now() / 1000)
+                };
+
+                myCache.set("producthunt", result);
+                res.json(result);
+            } else {
+                responseWithError('Bad status code received - ' + resultJSON.statusCode, res);
+            }
+
         });
+    } else {
+        res.json(value);
+    }
+
 };
