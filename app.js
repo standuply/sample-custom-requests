795483bc34f0ded495a603671ae1092e039a537a
Standuply <hello@standuply.com>
2017-10-04 12:18:30 +0700

    Reddit request added.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const server = restify.createServer();
 const producthuntRequest = require('./requests/producthunt');
 const hackernewsRequest = require('./requests/hackernews');
 const giphyRequest = require('./requests/giphy');
+const redditRequest = require('./requests/reddit');
 
 
 module.exports =  (options) => {
@@ -13,8 +14,11 @@ module.exports =  (options) => {
 
     server.get('/giphy', giphyRequest);
 
+    server.get('/reddit', redditRequest);
+    server.get('/reddit/:subreddit', redditRequest);
+
     server.listen(options.port, options.host, () => {
         console.log('%s listening at %s', server.name, server.url);
     });
-}
+};
 
