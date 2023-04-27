b216d66903eb18c4eac9d448604ef2b23bf9a252
Alex Kirpanyov <alexey.k@standuply.com>
2023-04-27 16:04:56 +0700

    Fix giphy custom request

--- requests/giphy.js
+++ requests/giphy.js
@@ -11,7 +11,9 @@ const processGiphy = (data) => {
     if (!data.data || !data.data.length) {
         throw 'GIPHY API error has occurred';
     }
+
     let gifURL;
+
     const num = Math.floor(Math.random() * data.data.length);
 
     if (data.data[num].images.fixed_height.size > 2500000) {
@@ -28,6 +30,7 @@ const processGiphy = (data) => {
 // This function gets 10 trending images from giphy.com
 module.exports = (req, res, next) => {
     let value = myCache.get('giphy');
+
     if (value !== undefined) {
         res.json(value);
         return;
@@ -40,19 +43,43 @@ module.exports = (req, res, next) => {
         .then(data => {
             const parseResult = processGiphy(data);
 
+            let blocks = [];
+
+            if (req.url.includes('messengerType=slack')) {
+                blocks = [
+                    {
+                        type: 'text',
+                        text: 'A trending gif from Giphy',
+                        markdown: false,
+                    },
+                    {
+                        type: 'image',
+                        url: parseResult.image,
+                        title: '',
+                        altText: 'Standuply trending gif from Giphy',
+                        color: 'green',
+                    }
+                ];
+            }
+
+            if (req.url.includes('messengerType=microsoft-teams')) {
+                blocks = [
+                    {
+                        type: 'text',
+                        text: '**A trending gif from Giphy**',
+                        markdown: false,
+                    },
+                    {
+                        type: 'image',
+                        url: parseResult.image,
+                        title: '',
+                        altText: 'Standuply trending gif from Giphy',
+                    }
+                ]
+            }
+
             // Make the Slack attachment - one object
-            const result = {
-                fallback: 'Giphy daily digest.',
-                color: '#36a64f',
-                pretext: 'A trending gif',
-                title: 'Giphy',
-                title_link: 'https://giphy.com/',
-                image_url: parseResult.image,
-                thumb_url: parseResult.thumbnail,
-                footer: 'Standuply',
-                footer_icon: 'https://app.standuply.com/img/16.png',
-                ts: Math.round(Date.now() / 1000)
-            };
+            const result = { blocks };
 
             myCache.set('giphy', result);
             res.json(result);
